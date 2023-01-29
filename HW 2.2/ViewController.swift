e987d3d296d51133de42bb19957773b1541a20c3
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-30 00:45:41 +0500

    Added objects to the view, links between objects and code.

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+enum Light {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var switchButton: UIButton!
+    
+    var burningLight = Light.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switchButton.layer.cornerRadius = 12
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
     }
 
-
+    @IBAction func switchButtonDidTapped() {
+        
+        switchButton.setTitle("NEXT", for: .normal)
+        
+        switch burningLight {
+        case .red:
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+            burningLight = .yellow
+        case .yellow:
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            burningLight = .green
+        case .green:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            burningLight = .red
+        }
+    }
+    
 }
 
