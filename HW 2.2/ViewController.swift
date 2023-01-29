c52f5ecefe486cb49b46cb2569d8cb115258ee59
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-29 23:57:17 +0500

    Initial Commit

---
+++ HW 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.2
+//
+//  Created by Игорь Солодянкин on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
